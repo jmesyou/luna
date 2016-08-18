633a0ebf3b5ced85b6b2ba2db34f876d744c9246
Darren Mei <darrenmei@hotmail.com>
2016-08-17 23:13:53 -0400

    Added some default functions to fold

--- src/fold.lua
+++ src/fold.lua
@@ -1,4 +1,5 @@
-List = require "list"
+local List = require "list"
+--local Errors = require "Errors"
 
 -- fold s.t. 1+2+3 -> 1+(2+3)
 function foldr(list, fn)
@@ -7,7 +8,7 @@ function foldr(list, fn)
 	if (#list == 2) then
 		return fn(head, tail[1])
 	end
-	
+
 	return fn(head, foldr(tail, fn))
 end
 
@@ -18,6 +19,38 @@ function foldl(list, fn)
 	if (#list == 2) then
 		return fn(tail[1], head)
 	end
-	
+
 	return fn(foldl(tail, fn), head)
+end
+
+function add(param1,param2)
+	return param1+param2
+end
+
+function sub(param1,param2)
+	return param1-param2
+end
+
+function exp(param1,param2)
+	return param1^param2
+end
+
+function mul(param1,param2)
+	return param1*param2
+end
+
+function div(param1,param2)
+	if (param2 != 0) then
+		return param1/param2
+	--if error checking is allowed
+	--else
+		--return Errors.new("Divide by zero error")
+	end
+	--if error checking is NOT allowed
+	--infinity in lua
+	if (mul(param1,param2) > 0) then
+		return 1e309
+	else
+		return -1e309
+	end
 end
