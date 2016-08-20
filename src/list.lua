eaefe3902cb4f072d07902410874cb1973b41a4f
Darren Mei <darrenmei@hotmail.com>
2016-08-20 10:23:41 -0400

    Improved table usage

--- src/list.lua
+++ src/list.lua
@@ -1,73 +1,62 @@
-
--- TODO Fold
-
--- DEPRECATED FUNCTIONS
--- Length -> default to #
-
-
 List = {}
-List.mt = {}
-List.mt.__index = List.mt
+List.__index = List
 
 function List.new(table)
 	local list = {}
 	list = table or {}
-	setmetatable(list, List.mt)
+	setmetatable(list, List)
 	return list
 end
 
-function List.append(list, obj)
-    assert(getmetatable(list) == List.mt)
-	table.insert(list, obj)
+function List.append(list, key)
+	assert(getmetatable(list) == List)
+	if(getmetatable(key) == List) then
+		List.concat(list, key)
+	else
+		table.insert(list, key)
+	end
 	return list
 end
 
 function List.cons(head, tail)
-    assert(getmetatable(tail) == List.mt)
-    table.insert(tail, head, 1)
-    return tail
+	assert(getmetatable(tail) == List)
+	table.insert(tail, head, 1)
+	return tail
 end
 
 function List.head(list)
-    assert(getmetatable(list) == List.mt)
+	assert(getmetatable(list) == List)
 	local obj = list[1]
 	return obj
 end
 
 function List.tail(list)
-    assert(getmetatable(list) == List.mt)
+	assert(getmetatable(list) == List)
 	local tail = List.new()
-    for i = 2, #list do
-        tail[i-1] = list[i]
-    end
+	for i = 2, #list do
+		tail[i-1] = list[i]
+	end
 	return tail
 end
 
 function List.concat(lst1, lst2)
+	assert(getmetatable(lst1) == List and getmetatable(lst2) == List)
 	for i = 1, #lst2 do
 		List.append(lst1,  lst2[i])
 	end
 	return lst1
 end
 
-function List.mt:__tostring()
-    local str = "["
+function List:__tostring()
+	local str = "["
 	for i = 1, #self-1 do
-		str = str .. tostring(self[i]) .. ", "
+		str = str .. self[i] .. ", "
 	end
 	return str .. self[#self] .. "]"
 end
 
 -- fold s.t. 1+2+3 -> 1+(2+3)
 function List.foldr(list, fn)
-	--[[
-	local head = List.head(list)
-	local tail = List.tail(list)
-	if (#list == 2) then
-		return fn(head, tail[1])
-	end
-	return fn(head, List.foldr(tail, fn))
-	--]]
 	accumulator = list[#list]
 	return foldrhelper(list, #list-1, fn, accumulator)
 
@@ -86,19 +75,11 @@ end
 -- fold s.t. 1+2+3 -> (1+2)+3
 -- folding only works if #list > 1
 function List.foldl(list, fn)
-	--[[
-	local head = List.head(list)
-	local tail = List.tail(list)
-	if (#list == 2) then
-		return fn(tail[1], head)
-	end
-	return fn(List.foldl(tail, fn), head)
-	]]--
 	accumulator = list[1]
 	return foldlhelper(list, 2, fn, accumulator)
 end
 
---tail recursion optimized & reduced object creation
+-- tail recursion optimized & reduced table creation
 function foldlhelper(list, headindex, fn, accumulator)
 	if (#list - headindex == 0) then
 		return fn(accumulator, list[headindex])
@@ -108,6 +89,7 @@ function foldlhelper(list, headindex, fn, accumulator)
 	return foldlhelper(list, headindex, fn, accumulator)
 end
 
+-- filter values from the list that comply with the function provided
 function List.filter(list, fn)
 	result = List.new()
 	for i=1, #list do
@@ -118,6 +100,7 @@ function List.filter(list, fn)
 	return result
 end
 
+-- apply a given function to all the elements in the list
 function List.map(list, fn)
 	local mappedlist = List.new()
 	for i=1, #list do
@@ -125,33 +108,5 @@ function List.map(list, fn)
 	end
 	return mappedlist
 end
-
-function add(x,y)
-	return x+y
-end
-
-function even(x)
-	return x % 2 == 0
-end
-
-function square(x)
-	return x*x
-end
-
-function test_cases()
-	local list = List.new()
-	for i = 1, 10 do
-		List.append(list,i)
-	end
-	print(List.foldl(list,add))
-	print(List.foldr(list,add))
-	print("\n")
-	print(List.foldl(list, function(x,y) return 2*x/y end))
-	print(List.foldr(list, function(x,y) return 2*x/y end))
-	--print(List.filter(list,even))
-	--print(List.map(list,square))
-end
-
-test_cases()
 
 return List
