ccc9f04d5952e85f5b7dd23e0f627fc0b79a18ff
Darren Mei <darrenmei@hotmail.com>
2016-08-20 09:40:55 -0400

    tail recursion on fold

--- src/list.lua
+++ src/list.lua
@@ -60,22 +60,52 @@ end
 
 -- fold s.t. 1+2+3 -> 1+(2+3)
 function List.foldr(list, fn)
+	--[[
 	local head = List.head(list)
 	local tail = List.tail(list)
 	if (#list == 2) then
 		return fn(head, tail[1])
 	end
 	return fn(head, List.foldr(tail, fn))
+	--]]
+	accumulator = list[#list]
+	return foldrhelper(list, #list-1, fn, accumulator)
+
+end
+
+-- essentially do a foldl starting at end instead of beginning
+function foldrhelper(list, headindex, fn, accumulator)
+	if(headindex == 1) then
+		return fn(list[headindex], accumulator)
+	end
+	accumulator = fn(list[headindex], accumulator)
+	headindex = headindex - 1
+	return foldrhelper(list, headindex, fn, accumulator)
 end
 
 -- fold s.t. 1+2+3 -> (1+2)+3
+-- folding only works if #list > 1
 function List.foldl(list, fn)
+	--[[
 	local head = List.head(list)
 	local tail = List.tail(list)
 	if (#list == 2) then
 		return fn(tail[1], head)
 	end
 	return fn(List.foldl(tail, fn), head)
+	]]--
+	accumulator = list[1]
+	return foldlhelper(list, 2, fn, accumulator)
+end
+
+--tail recursion optimized & reduced object creation
+function foldlhelper(list, headindex, fn, accumulator)
+	if (#list - headindex == 0) then
+		return fn(accumulator, list[headindex])
+	end
+	accumulator = fn(accumulator, list[headindex])
+	headindex = headindex + 1
+	return foldlhelper(list, headindex, fn, accumulator)
 end
 
 function List.filter(list, fn)
@@ -109,10 +139,17 @@ function square(x)
 end
 
 function test_cases()
-	local list = List.new({1,2,3,4,5,6,7,8,9,10})
+	local list = List.new()
+	for i = 1, 10 do
+		List.append(list,i)
+	end
 	print(List.foldl(list,add))
-	print(List.filter(list,even))
-	print(List.map(list,square))
+	print(List.foldr(list,add))
+	print("\n")
+	print(List.foldl(list, function(x,y) return 2*x/y end))
+	print(List.foldr(list, function(x,y) return 2*x/y end))
+	--print(List.filter(list,even))
+	--print(List.map(list,square))
 end
 
 test_cases()
