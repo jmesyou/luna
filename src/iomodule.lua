3a2fb517493ebd4947d46b0008e6e7bd095cc934
Darren Mei <darrenmei@hotmail.com>
2016-08-23 19:13:09 -0400

    Fixed iomodule.lua

--- src/iomodule.lua
+++ src/iomodule.lua
@@ -1,6 +1,10 @@
-function readFile(filename)
+IOModule = {}
+
+function IOModule.readFile(filename)
 	local f = io.open(filename, "rb")
 	local data = f:read("*a")
 	f:close()
 	return data
 end
+
+return IOModule
--- src/mune.lua
+++ src/mune.lua
@@ -1,16 +1,31 @@
 local env = require "env"
+local ioreader = require "iomodule"
 
-function repl()
-    while true do
-        print(env.evaluate(io.read()))
+function repl(string)
+    if (string == nil) then
+        while true do
+            print(env.evaluate(io.read()))
+        end
+    elseif (type(string) == "string") then
+        print(env.evaluate(string))
     end
 
 end
+
 
 if #arg == 0 then
-    repl()
+    repl(nil)
 else
+    --TODO implement multiple line reads && support for partial expressions
     for i = 1, #arg do
-        print(env.evaluate(iomodule.readFile(arg[i])))
+        repl(ioreader.readFile(arg[i]))
     end
 end
+
+
+stringarr = {}
+stringarr[1] = "hello"
+stringarr[2] = "world"
+
+print(type(stringarr))
+print(type({"hello, world"}))
